b620a343531af815dad68a1f55454e426b8c89eb
Nathan <nvm7252@rit.edu>
2026-06-25 23:08:04 -0400

    Got a window to open

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,81 @@
+#if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
+#include <vulkan/vulkan_raii.hpp>
+#else
+import vulkan_hpp;
+#endif
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
+
 
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <memory>
 
-int main() {
-    if (!glfwInit()) {
-        std::cerr << "Failed to init GLFW\n";
-        return 1;
+class HelloTriangleApplication {
+public:
+
+    GLFWwindow* window;
+    static constexpr uint32_t WIDTH = 800;
+    static constexpr uint32_t HEIGHT = 600;
+
+
+    void run() {
+        InitWindow();
+        InitVulkan();
+        mainLoop();
+        cleanup();
     }
 
-    if (!glfwVulkanSupported()) {
-        std::cerr << "Vulkan not supported\n";
-        return 1;
+private:
+    void InitWindow() {
+        // Init GLFW library
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        // Disable resized windows for now
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        // Create actual window
+        window = glfwCreateWindow(
+            WIDTH,
+            HEIGHT,
+            "Vulkan",   // Title
+            nullptr,    // Primary monitor for fullscreen
+            nullptr);   // Handle to another shared window
     }
 
-    std::cout << "Vulkan is supported. Ready to go.\n";
+    void InitVulkan() {
 
-    glfwTerminate();
-    return 0;
+    }
+
+    void mainLoop() {
+        // Keep going?
+        while(!glfwWindowShouldClose(window)){
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        // Destroy window and GLFW
+        glfwDestroyWindow(window);
+        glfwTerminate();
+
+    }
+};
+
+int main()
+{
+    try
+    {
+        HelloTriangleApplication app;
+        app.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
