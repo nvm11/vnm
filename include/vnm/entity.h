f5bfc144817a39adfd506a2a7926048de832bff2
Nathan <nvm7252@rit.edu>
2026-07-29 20:17:52 -0400

    added some getters and setters to entity

--- include/vnm/entity.h
+++ include/vnm/entity.h
@@ -1,12 +1,25 @@
 #include <string>
 #include <vector>
 #include <memory>
-#include "component.h"
+#include "Component.h"
 
 class Entity
 {
 private:
+    // Fields
     std::string name;
     bool active = true;
     std::vector<std::unique_ptr<Component>> components;
+
+public:
+    // Explicit keyword requires exact type matching
+    // Constructor
+    explicit Entity(const std::string& name);
+
+    // Getters
+    const std::string& GetName();
+    bool IsActive();
+    
+    // Setters
+    void SetActive(bool active);
 };
